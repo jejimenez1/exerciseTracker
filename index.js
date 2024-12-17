bfc8e0e17050a7b181f636f081de7898f7a5ffbd
Jesus Jimenez <jesujimenezochoa@gmail.com>
2024-12-17 16:46:17 +0000

    first

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-require('dotenv').config()
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 /* const express = require('express')
 const app = express()
